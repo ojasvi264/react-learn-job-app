d946018a38519d14f84a1c3021b8ae455592e222
ojasvi264 <lalamdorje@gmail.com>
2025-03-28 10:00:30 +0900

    refactors components accordingly to add multiple pages.

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,23 +1,48 @@
-import jobs from '../jobs.json'
-import Job from './Job'
+import { useState, useEffect } from "react";
+import Job from "./Job";
+import Spinner from "./Spinner";
 
-const JobListing = () => {
+const JobListing = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiURL = isHome ? '/api/jobs?_limit=3' : '/api//jobs'
+      try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        setJobs(data);
+      } catch (error) {
+        console.error("Error fetching jobs", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
-    <div className="container-xl lg:container m-auto">
-      <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        Browse Jobs
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
+        </h2>
+
         {
-            jobs.map((job, index) => (
-                <Job key = { index } job = { job } />
-            ))
+          loading ? (
+            <Spinner loading={loading} />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job, index) => (
+                <Job key={index} job={job} />
+              ))}
+            </div>
+          )
         }
       </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default JobListing
+export default JobListing;
