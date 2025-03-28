679f0cde59e38234bd46ebc1572d5b75264c5f3b
ojasvi264 <lalamdorje@gmail.com>
2025-03-28 12:14:02 +0900

    refactors onClick events.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,19 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaMapMarker } from "react-icons/fa";
+import { toast } from "react-toastify";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
   const job = useLoaderData();
+  const naviagte = useNavigate();
+
+  const handleDeleteJob = async (jobId) => {
+    const confirm = window.confirm("Are you sure you want to delete this job?");
+    if(!confirm) return;
+    deleteJob(jobId);
+    toast.success("Job deleted successfully");
+    naviagte('/jobs');
+  };
 
   return (
     <>
@@ -72,12 +82,12 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button  onClick={() => handleDeleteJob(job.id)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
               </div>
